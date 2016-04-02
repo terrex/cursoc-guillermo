06c2c371c7504cfa5634f94842a817b757a2d58b
Guillermo Gutierrez <xiterrex@gmail.com>
2016-04-02 22:56:00 +0200

    Tema 3 Ejercicio 1.3 cadenas

--- ejercicios/tema3/1.3.cadenas.c
+++ ejercicios/tema3/1.3.cadenas.c
@@ -3,12 +3,18 @@
 
 int main()
 {
-	char *cadena = "the quick brown fox jumps over the lazy dog";
-	printf("Original: %s (%d)\n", cadena, strlen(cadena));
+	char cadena[] = "the quick brown fox jumps over the lazy dog";
+	printf("Original: %s (%lu)\n", cadena, strlen(cadena));
 
-	for (int i = 0; i < strlen(cadena) -1; i++)
-		cadena[i] = cadena[i] - 0x20;
-	//printf("Todo mayusculas: %s\n", cadena);
+	for (int i = 0; i < strlen(cadena); i++)
+		if (cadena[i] != ' ')
+			cadena[i] = (char) (cadena[i] - 0x20);
+	printf("Todo mayusculas: %s\n", cadena);
+
+	for (int i = 1; i < strlen(cadena); i++)
+		if (cadena[i - 1] != ' ' && cadena[i] != ' ')
+			cadena[i] = (char) (cadena[i] + 0x20);
+	printf("Estilo Titulo: %s\n", cadena);
 
 	return 0;
 }
