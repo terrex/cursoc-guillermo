8c0ea1533b4a2b3f0787ffdba32f9a2d43de5608
Guillermo Gutiérrez <xiterrex@gmail.com>
2016-04-03 20:05:45 +0200

    Tema 4 Ejercicio 1.1 Person

---
+++ ejercicios/tema4/1.1.person.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+struct person
+{
+	char name[256];
+	char surname[256];
+	unsigned char age;
+	unsigned int phone;
+};
+
+enum person_attr
+{
+	NAME,
+	SURNAME,
+	AGE,
+	PHONE
+};
+
+void print_person(const struct person *p);
+void print_attr(const struct person *p, enum person_attr attr);
+
+void print_person(const struct person *p)
+{
+	printf("PERSONA(%s %s, %d años, teléfono %d)\n",
+		p->name, p->surname, p->age, p->phone);
+}
+
+void print_attr(const struct person *p, enum person_attr attr)
+{
+	switch(attr) {
+	case NAME:
+		printf("%s", p->name);
+		break;
+	case SURNAME:
+		printf("%s", p->surname);
+		break;
+	case AGE:
+		printf("%d", p->age);
+		break;
+	case PHONE:
+		printf("%d", p->phone);
+		break;
+	default:
+		printf("Error: %s:%d", __FILE__, __LINE__);
+	}
+}
+
+
+int main()
+{
+	struct person persona = {"Juan", "Tanamera", 23, 956123123};
+
+	print_person(&persona);
+
+	print_attr(&persona, PHONE);
+
+	printf("\n");
+
+	return 0;
+}
---
+++ tema4/campos-bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+
+union float_t
+{
+	float f;
+	struct {
+		uint32_t fra : 23;
+		uint32_t exp : 8;
+		uint32_t sig : 1;
+	};
+};
+
+int main()
+{
+	union float_t f;
+	f.f = -3.1416;
+	printf("sizeof(f) = %lu\n", sizeof(f));
+	printf("signo     = %u\n", f.sig);
+	printf("exponente = %d\n", f.exp);
+	printf("fraccion  = %d\n", f.fra);
+	return 0;
+}
