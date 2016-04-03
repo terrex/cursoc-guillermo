f36484e71fca9fe7e0d3a477b8c00e3a2ca60b60
Guillermo Gutiérrez <xiterrex@gmail.com>
2016-04-03 15:35:31 +0200

    tema 3 ejercico 2.2 punteros y arrays (sin terminar)

---
+++ ejercicios/tema3/2.2.arrays.punteros.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int A[5] = {0, 1, 2, 3, 4};
+	float B[5] = {10.1, 10.2, 10.3, 10.4, 10.5};
+	float C[5];
+	
+	int *A_p = A;
+	float *B_p = B;
+
+	// forma1 ?
+	
+	//forma2 ?
+	
+	//forma 3?
+	
+
+	return 0;
+}
