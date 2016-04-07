f96f21f671c21a3629db7d7e0349d4d0c543b39c
Guillermo Gutiérrez <xiterrex@gmail.com>
2016-04-07 18:21:02 +0200

    corrigiendo

--- ejercicios/tema3/2.3.punteros.matrices.c
+++ ejercicios/tema3/2.3.punteros.matrices.c
@@ -4,19 +4,13 @@ int main()
 {
 	int A[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-	// puntero a matriz bidimensional: es correcto pero innecesario
-	//int ***A_p = (int ***) (&A);
-	// mejor un alias a A
-	int **A_p = (int **) (A);
-
+	// puntero a matriz bidimensional
+	int ***A_p = (int ***) (&A);
 	int i, j;
 
 	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			// es correcto
-			// printf("  %04d", *(*(A_p + i) + j));
-			// y así también
-			printf("  %04d", (int) *(A_p + 3*i + j));
+		for (j = 0; j < 3; j++) {	
+			printf("  %04d", *(*(A + i) + j));
 		}
 		printf("\n");
 	}
