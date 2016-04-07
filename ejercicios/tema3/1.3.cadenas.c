fbcc8457d58a6674611ec6c8d5ccd756209d2ff5
Guillermo Gutiérrez <xiterrex@gmail.com>
2016-04-07 17:59:08 +0200

    corrigiendo ejercicios

--- ejercicios/tema3/1.3.cadenas.c
+++ ejercicios/tema3/1.3.cadenas.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 int main()
 {
 	char cadena[] = "the quick brown fox jumps over the lazy dog";
+	//char cadena[] = "the quick brown fox, jumps over the lazy dog";
+	//char cadena[] = "t";
+	//char cadena[] = "";
 	printf("Original: %s (%lu)\n", cadena, strlen(cadena));
 
 	for (int i = 0; i < strlen(cadena); i++)
-		if (cadena[i] != ' ')
+		if (cadena[i] >= 'a' && cadena[i] <= 'z')
 			cadena[i] = (char) (cadena[i] - 0x20);
 	printf("Todo mayusculas: %s\n", cadena);
 
-	for (int i = 1; i < strlen(cadena); i++)
-		if (cadena[i - 1] != ' ' && cadena[i] != ' ')
-			cadena[i] = (char) (cadena[i] + 0x20);
+	bool espacio_anterior = true;
+	for (int i = 0; i < strlen(cadena); i++) {
+		if (espacio_anterior) {
+			if (cadena[i] >= 'a' && cadena[i] <= 'z')
+				cadena[i] = (char) (cadena[i] - 0x20);
+		} else {
+			if (cadena[i] >= 'A' && cadena[i] <= 'Z')
+				cadena[i] = (char) (cadena[i] + 0x20);
+		}
+
+		espacio_anterior = (bool) (cadena[i] == ' ');
+	}
 	printf("Estilo Titulo: %s\n", cadena);
 
 	return 0;
--- ejercicios/tema3/2.3.punteros.matrices.c
+++ ejercicios/tema3/2.3.punteros.matrices.c
@@ -4,13 +4,19 @@ int main()
 {
 	int A[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-	// puntero a matriz bidimensional
-	int ***A_p = (int ***) (&A);
+	// puntero a matriz bidimensional: es correcto pero innecesario
+	//int ***A_p = (int ***) (&A);
+	// mejor un alias a A
+	int **A_p = (int **) (A);
+
 	int i, j;
 
 	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {	
-			printf("  %04d", *(*(A + i) + j));
+		for (j = 0; j < 3; j++) {
+			// es correcto
+			// printf("  %04d", *(*(A_p + i) + j));
+			// y así también
+			printf("  %04d", (int) *(A_p + 3*i + j));
 		}
 		printf("\n");
 	}
